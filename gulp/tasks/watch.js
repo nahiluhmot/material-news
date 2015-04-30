052ebcac9e1bf52045189c8ef15ca00abbda1865
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-30 11:32:06 -0400

    Fix the scss watch matcher

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,11 @@ var gulp = require('gulp');
  * Watch the sources and rebuild the assets when they change.
  */
 gulp.task('watch', ['build'], function() {
+  var scss = config.scss.copy.map(function(conf) {
+    return conf.src;
+  });
+
   gulp.watch(config.html.src, ['html']);
   gulp.watch(config.js.compile.src, ['js']);
-  gulp.watch(config.scss.src, ['scss']);
+  gulp.watch(scss, ['scss']);
 });
