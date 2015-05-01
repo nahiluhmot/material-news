6aead35937cae6def3fa3ca9005a4cb4a58de5ea
Tom Hulihan <hulihan.tom159@gmail.com>
2015-05-01 10:49:55 -0400

    Implement the remainder of the stories controller

--- app/controllers/stories.js
+++ app/controllers/stories.js
@@ -1,28 +1,21 @@
-import Top from 'components/pages/stories/top';
+import View from 'components/pages/stories';
 import { ITEMS_PER_PAGE } from 'config/constants';
-
 import API from 'requests/hacker-news';
-
 import render from 'services/render';
 import Paginator from 'services/paginator';
-
 import { navigate } from 'aviator';
 
-const loadPage = (name, promise, page) => {
-  if ((typeof page !== 'number') || (page < 1)) {
-    page = 1;
-  }
-  page = Math.ceil(page);
-
+const loadPage = promise => {
   promise.then(ids => {
-    const toLoad = ids.map(id => () => API.findItemById(id));
-    const paginator = new Paginator(ITEMS_PER_PAGE, toLoad);
-    return paginator.getPage(0);
-  }).then(page => {
-    console.log(`Loaded the ${name} stories`);
-    console.log(page);
+    const promises = ids.map(id => () => API.findItemById(id));
+    const pages = new Paginator(ITEMS_PER_PAGE, promises);
+
+    render(View, {
+      getItemsByPage: page => pages.getPage(page),
+      lastPage: pages.pageCount()
+    });
   }).catch(error => {
-    console.log(`Unable to load the ${name} stories`);
+    console.log('Unable to fetch the stories');
     console.log(error);
   });
 };
@@ -34,41 +27,27 @@ const Stories = {
   /**
    * Get the top stories -- like hot on reddit.
    */
-  top: () => {
-    API.topStories().then(ids => {
-      const promises = ids.map(id => () => API.findItemById(id));
-      const pages = new Paginator(ITEMS_PER_PAGE, promises);
-
-      render(Top, {
-        getItemsByPage: page => pages.getPage(page),
-        lastPage: pages.pageCount()
-      });
-    }).catch(error => {
-      console.log('Unable to fetch the top stories');
-      console.log(error);
-    });
-  },
+  top: () => loadPage(API.topStories()),
 
   /**
    * Get the newest stories.
    */
-  recent: ({ queryParams }) =>
-    loadPage('new', API.newStories(), queryParams.page),
+  recent: () => loadPage(API.newStories()),
+
   /**
    * Get the Ask HN stories.
    */
-  ask: ({ queryParams }) =>
-    loadPage('ask', API.askStories(), queryParams.page),
+  ask: () => loadPage(API.askStories()),
+
   /**
    * Get the Show HN stories.
    */
-  show: ({ queryParams }) =>
-    loadPage('show', API.showStories(), queryParams.page),
+  show: () => loadPage(API.showStories()),
+
   /**
    * Get the Job stories.
    */
-  job: ({ queryParams }) =>
-    loadPage('job', API.jobStories(), queryParams.page)
+  job: () => loadPage(API.jobStories()),
 };
 
 export default Stories;
