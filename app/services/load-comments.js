bfbd532cf4147999825b868386e227618ca4e91a
Tom Hulihan <hulihan.tom159@gmail.com>
2015-05-04 08:04:46 -0400

    Add a max depth to the load comments service

--- app/services/load-comments.js
+++ app/services/load-comments.js
@@ -1,4 +1,4 @@
-import { map } from 'bluebird';
+import { all, map } from 'bluebird';
 import { compact, extend } from 'underscore';
 
 import { findItemById } from 'services/hacker-news';
@@ -9,13 +9,15 @@ import { validComment } from 'services/validators';
  * Hacker News API, as well as their children, recursively. The children can be
  * found in the loadedChildren key of the comment.
  */
-const recur = item =>
-  map(item.kids || [], id => findItemById(id).catch(() => null))
-    .then(compact)
-    .filter(validComment)
-    .map(comment =>
-      recur(comment)
-        .then(children =>
-          extend({ loadedChildren: children }, comment)));
+const recur = (item, depth) =>
+  (depth === 0) ?
+    all([]) :
+    map(item.kids || [], id => findItemById(id).catch(() => null))
+      .then(compact)
+      .filter(validComment)
+      .map(comment =>
+        recur(comment, depth - 1)
+          .then(children =>
+            extend({ loadedChildren: children }, comment)));
 
 export default recur;
