29d7b456bfb489b4a85e01697af42cc0659bd6a0
Tom Hulihan <hulihan.tom159@gmail.com>
2015-05-07 08:14:03 -0400

    Cleaner stories controller

--- app/controllers/stories.js
+++ app/controllers/stories.js
@@ -6,24 +6,6 @@ import Paginator from 'services/paginator';
 import { validStory }  from 'services/validators';
 import { navigate } from 'aviator';
 
-const loadPage = (type, promise) => {
-  promise.then(ids => {
-    const promises = ids.map(id => () => API.findItemById(id));
-    const pages = new Paginator(ITEMS_PER_PAGE, promises);
-
-    render(List, {
-      getItemsByPage: page => pages.getPage(page).filter(validStory),
-      lastPage: pages.pageCount()
-    });
-  }).catch(error => {
-    navigate('/errors/', {
-      queryParams: {
-        message: `Unable to load ${type}`
-      }
-    });
-  });
-};
-
 /**
  * This object contains the routing logic for loading the stories views.
  */
@@ -31,27 +13,59 @@ const Stories = {
   /**
    * Get the top stories -- like hot on reddit.
    */
-  top: () => loadPage('top stories', API.topStories()),
+  top() {
+    this._loadPage('top stories', API.topStories());
+  },
 
   /**
    * Get the newest stories.
    */
-  recent: () => loadPage('recent stories', API.newStories()),
+  recent() {
+    this._loadPage('recent stories', API.newStories());
+  },
 
   /**
    * Get the Ask HN stories.
    */
-  ask: () => loadPage('ask stories', API.askStories()),
+  ask() {
+    this._loadPage('ask stories', API.askStories());
+  },
 
   /**
    * Get the Show HN stories.
    */
-  show: () => loadPage('ask stories', API.showStories()),
+  show() {
+    this._loadPage('ask stories', API.showStories());
+  },
 
   /**
    * Get the Job stories.
    */
-  job: () => loadPage('job listings', API.jobStories()),
+  job() {
+    this._loadPage('job listings', API.jobStories());
+  },
+
+  /**
+   * Private helpers.
+   */
+
+  _loadPage(type, promise) {
+    promise.then(ids => {
+      const promises = ids.map(id => () => API.findItemById(id));
+      const pages = new Paginator(ITEMS_PER_PAGE, promises);
+
+      render(List, {
+        getItemsByPage: page => pages.getPage(page).filter(validStory),
+        lastPage: pages.pageCount()
+      });
+    }).catch(error => {
+      navigate('/errors/', {
+        queryParams: {
+          message: `Unable to load ${type}`
+        }
+      });
+    });
+  }
 };
 
 export default Stories;
